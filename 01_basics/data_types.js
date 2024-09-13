0cb438967639d5f2dbd2cfdebc429a9385f4cb9e
Kumar Shaiv Sah <161468027+zshaiv@users.noreply.github.com>
2024-09-13 12:47:58 +0000

    container config done

---
+++ 01_basics/data_types.js
@@ -0,0 +1,11 @@
+"use strict"// treat all JS code as newer version
+
+//alert(3+3)// we are using Nodejs not browser
+
+console.log(3+3) 
+console.log("shaiv")
+
+let name="shaiv"
+let age="18"
+let isLoggedIn= false 
+console.log(age)
