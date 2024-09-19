d98c49f5a46cb16e5fb6ac5c20543c897d65d5db
Kumar Shaiv Sah <161468027+zshaiv@users.noreply.github.com>
2024-09-19 01:27:25 +0000

    Conversion and Operations of datatypes

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -1,27 +1,45 @@
-// let score = undefined;
+let score = undefined;
 
-// console.log(typeof (score));
+console.log(typeof (score));
 
-// let valueInNumber = Number(score)
-// console.log(typeof (valueInNumber));
-// console.log(valueInNumber);
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
 
-//"33"=> 33
-//"33abc"=> NaN
-//true => 1; false=>0
+// "33"=> 33
+// "33abc"=> NaN
+// true => 1; false=>0
 
 let isLoggedIn=1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 console.log(booleanIsLoggedIn);
 
-//1=>true; 0=>false;
-//"" => false;
-//"shaiv" => true;
+// 1=>true; 0=>false;
+// "" => false;
+// "shaiv" => true;
 
 let x = 33;
 
 let stringNumber= String(x);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// **********************Operations**************************
+
+let value =3;
+let negValue= -value
+console.log(negValue)
+
+let str1= "hello";
+let str2="shaiv";
+
+let str3= str1+str2;
+console.log(str3);
+
+let counter= 100;
+counter++;
+console.log(counter);
+
+
 
