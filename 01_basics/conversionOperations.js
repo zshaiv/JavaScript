29548ff45ed7e9e2c82db405f2d22a1ca9963c01
Kumar Shaiv Sah <161468027+zshaiv@users.noreply.github.com>
2024-09-19 01:13:36 +0000

    Conversion of datatypes

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,27 @@
+// let score = undefined;
+
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+//"33"=> 33
+//"33abc"=> NaN
+//true => 1; false=>0
+
+let isLoggedIn=1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1=>true; 0=>false;
+//"" => false;
+//"shaiv" => true;
+
+let x = 33;
+
+let stringNumber= String(x);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
