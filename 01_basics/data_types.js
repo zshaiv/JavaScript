99357486678ad082ca79d93740548c3a3a589ecc
Kumar Shaiv Sah <161468027+zshaiv@users.noreply.github.com>
2024-09-26 11:40:34 +0000

    data types

--- 01_basics/data_types.js
+++ 01_basics/data_types.js
@@ -9,3 +9,36 @@ let isLoggedIn= false
 
 console.log(typeof undefined) //output: undefined
 console.log(typeof null) //output: object
+
+
+//primitive
+// 7 types : string, number, boolean, null, undefined, symbol, BigInt
+
+const score =100
+const scoreValue= 100.3
+
+const isLoggedin=false
+const outsideTemp= null
+let useremail;
+
+const id = Symbol('123')
+const anotherID= Symbol('123')
+
+console.log(id=== anotherID);
+const bigNumber= 3219381938933n
+
+//reference (non primitive)
+//array, objects, functions
+
+const heroes=["shaktimaan", "nagraj", "doga"]
+let myObj={
+    name:"shaiv",
+    age: 18,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+
+}
+
+console.log(typeof scoreValue)
