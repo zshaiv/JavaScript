ed50e0cadcbccb07d038495a9a39714b302d5e7c
Kumar Shaiv Sah <161468027+zshaiv@users.noreply.github.com>
2024-10-02 08:19:05 +0000

    stacks and heaps

---
+++ 01_basics/stacks_heaps.js
@@ -0,0 +1,23 @@
+// stacks are used in primitive data types
+// heaps are used in non primitive data types
+
+let myYTname= "zshaiv";
+
+let anothername=  myYTname;
+anothername= "zidane";
+
+console.log(myYTname);
+console.log(anothername);
+
+let user_1={    
+    email: "zshaiv@js.com",
+    UPI: "zshaiv@ybl"
+}
+
+let user_2= user_1
+
+user_2.email="amaan.@js.com"
+
+console.log(user_1.email);
+console.log(user_2.email);
+
